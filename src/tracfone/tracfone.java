7628b5b866eafed065e3d7e10ad1734741cc22c4
Francisco <martinezfran63@gmail.com>
2016-11-02 14:50:44 -0600

    tracfone empty. ready to add code

--- src/tracfone/tracfone.java
+++ src/tracfone/tracfone.java
@@ -16,11 +16,12 @@ public class tracfone{
 	
 	public static WebDriver mydriver;
 	
+	
 	@BeforeMethod
 
 	  public void beforeMethod() throws Exception {
 		//Test Time and Date
-		System.out.println("Straight Talk Catalog Test Executed: " + new java.util.Date());
+		System.out.println("Tracfone Test Executed: " + new java.util.Date());
 		
 		
 	}
@@ -48,7 +49,7 @@ public class tracfone{
 	
 	 
 	Bright_Point_Price = "0";
-	myURL="http://specials.straighttalk.com/catalog/";
+	myURL="http://sale.tracfone.com/";
 	zip1="80301";
 
 
@@ -62,122 +63,9 @@ public class tracfone{
 	//take screenshot to make sure content is displaying corretly
 	PrintScreen.main(mydriver);
 	
-	
-	//Type in the Zipcode
-	//System.out.println("Type in the Zipcode");
-	mydriver.findElement(By.xpath("html/body/div[4]/div[2]/form/input[1]")).sendKeys(zip1);
 
-	//click the “Enter” button
-	//System.out.println("click the “Enter” button");
-	mydriver.findElement(By.xpath("/html/body/div[4]/div[2]/form/input[11]")).click();
-
-	//click the "Yes" button
-	//System.out.println("click the "Yes" button");
-	TimeUnit.SECONDS.sleep(2);
-	mydriver.findElement(By.xpath("/html/body/div[6]/div[1]/div/a")).click();
-
-	
-	// Get the text of the body element
-	WebElement body = mydriver.findElement(By.tagName("body"));
-	String bodyText = body.getText();
-
-	// Count occurrences of the string
-	count = 0;
-
-	// search for the string "VIEW DETAILS" within the text. when instance of string "VIEW DETAILS" found, increment 'phone_count'
-	while (bodyText.contains("VIEW DETAILS")) {
-		count++;
-		// continue searching from where you left off
-		bodyText = bodyText.substring(bodyText.indexOf("VIEW DETAILS") + "VIEW DETAILS".length());
-	}
 		
-	
-	//Prompt informing user of number of devices on page
-	System.out.println("There are " + count + " phones currently on the page\n");
-
-
-	//i is device count. set to device your having error at so you don't have to go through each device during execution
-	// i=1 to start from begining
-	for(int i = 55; i<=count ;i++){
-	
-	mydriver.navigate() .to(myURL);
-			
-	TimeUnit.SECONDS.sleep(3);
-	
-					if ((i > 3 ) ) {
 						
-						JavascriptExecutor jse = (JavascriptExecutor)mydriver;
-						jse.executeScript("window.scrollBy(0,455)", "");	
-					}
-					
-					if ((i > 6 ) ) {
-						
-						JavascriptExecutor jse = (JavascriptExecutor)mydriver;
-						jse.executeScript("window.scrollBy(0,450)", "");
-					}
-					
-					if ((i > 9 ) ) {
-					
-						JavascriptExecutor jse = (JavascriptExecutor)mydriver;
-						jse.executeScript("window.scrollBy(0,450)", "");
-					}
-					
-					if ((i > 12 ) ) {
-						
-						JavascriptExecutor jse = (JavascriptExecutor)mydriver;
-						jse.executeScript("window.scrollBy(0,450)", "");
-					}
-					
-					if ((i > 15 ) ) {
-					
-						JavascriptExecutor jse = (JavascriptExecutor)mydriver;
-						jse.executeScript("window.scrollBy(0,450)", "");
-					}
-					
-					if ((i > 18 ) ) {
-					
-						JavascriptExecutor jse = (JavascriptExecutor)mydriver;
-						jse.executeScript("window.scrollBy(0,450)", "");
-					}
-					
-					if ((i > 21 ) ) {
-					
-						JavascriptExecutor jse = (JavascriptExecutor)mydriver;
-						jse.executeScript("window.scrollBy(0,450)", "");
-						}
-					
-					if ((i > 24 ) ) {
-						
-						JavascriptExecutor jse = (JavascriptExecutor)mydriver;
-						jse.executeScript("window.scrollBy(0,450)", "");
-					}
-					
-					if ((i > 27 ) ) {
-					
-						JavascriptExecutor jse = (JavascriptExecutor)mydriver;
-						jse.executeScript("window.scrollBy(0,450)", "");		
-					}
-		
-					
-			TimeUnit.SECONDS.sleep(3);
-			
-			
-			
-			// Select the Name of the Phone
-			Name=mydriver.findElement(By.xpath("html/body/section[1]/div[3]/ul/li["+i+"]/div[3]/div")).getText();
-
-			//Select the Price of phone on Straight talk  phones page
-			Catalog_Price=mydriver.findElement(By.xpath("html/body/section[1]/div[3]/ul/li["+i+"]/div[4]")).getText();
-			
-			
-		   //Prompt informing user of number of device #, device name, device price
-		   System.out.println("#"+ i+ " " + "Straight Talk Catalog:"+ Name+"\n");
-		   
-		   
-		   
-		
-		
-							regular.main( mydriver,  i,  Bright_Point_Price,  Catalog_Price,  Name);
 							
 							
 							
